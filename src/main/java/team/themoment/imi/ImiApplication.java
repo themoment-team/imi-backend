5f498db164b45f1ae7e5197b250d1fc2fb412fe6
wwwcomcomcomcom <iieiiergn@gmail.com>
2025-03-10 17:16:33 +0900

    init COMMIT

---
+++ src/main/java/team/themoment/imi/ImiApplication.java
@@ -0,0 +1,13 @@
+package team.themoment.imi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/themoment/imi/ImiApplicationTests.java
@@ -0,0 +1,13 @@
+package team.themoment.imi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
