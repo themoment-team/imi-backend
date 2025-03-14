f1257e8f854f8c4be65605b9c902646b44db0746
KIM TAEEUN <snowykte0426@naver.com>
2025-03-14 19:57:26 +0900

    update: enhance JWT authorization filter to use CustomUserDetails

--- src/main/java/team/themoment/imi/global/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/team/themoment/imi/global/security/filter/JwtAuthorizationFilter.java
@@ -7,10 +7,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
+import team.themoment.imi.global.security.auth.CustomUserDetails;
 import team.themoment.imi.global.security.jwt.service.JwtParserService;
 
 import java.io.IOException;
@@ -18,20 +17,28 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
-    private final UserDetailsService userDetailsService;
     private final JwtParserService jwtParserService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        if (request.getRequestURI().startsWith("/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
         String token = request.getHeader("Authorization");
         if (token != null && token.startsWith("Bearer ")) {
             token = token.substring(7);
             if (jwtParserService.validateToken(token)) {
                 String username = jwtParserService.extractUserId(token);
-                UserDetails userDetails = User.withUsername(username).build();
-                SecurityContextHolder.getContext().setAuthentication(
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()));
+                if (username != null) {
+                    UserDetails userDetails = new CustomUserDetails(username);
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails, null, userDetails.getAuthorities()
+                            );
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
             }
         }
         filterChain.doFilter(request, response);
