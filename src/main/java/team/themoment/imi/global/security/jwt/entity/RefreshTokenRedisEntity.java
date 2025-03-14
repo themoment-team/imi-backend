86dda72af69ef19f9db1d9b234a93cd429eec427
KIM TAEEUN <snowykte0426@naver.com>
2025-03-14 15:19:28 +0900

    add: defining refresh token redis entity

---
+++ src/main/java/team/themoment/imi/global/security/jwt/entity/RefreshTokenRedisEntity.java
@@ -0,0 +1,18 @@
+package team.themoment.imi.global.security.jwt.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@RedisHash(value = "refresh_token")
+@Builder
+@Getter
+public class RefreshTokenRedisEntity {
+    @Id
+    private String refreshToken;
+    @Indexed
+    private String userId;
+    private long expiration;
+}
