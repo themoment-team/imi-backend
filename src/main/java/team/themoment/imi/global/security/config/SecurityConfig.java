048d8ead6776dd34956f78e6ff9caf8b31a63850
KIM TAEEUN <snowykte0426@naver.com>
2025-03-14 15:32:39 +0900

    chore: security settings

---
+++ src/main/java/team/themoment/imi/global/security/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package team.themoment.imi.global.security.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import team.themoment.imi.global.security.filter.JwtAuthorizationFilter;
+import team.themoment.imi.global.security.jwt.service.JwtParserService;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final JwtParserService jwtParserService;
+    private final DomainAuthorizationConfig domainAuthorizationConfig;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        domainAuthorizationConfig.configure(http);
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(
+                        new JwtAuthorizationFilter(userDetailsService, jwtParserService),
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .exceptionHandling(exceptions -> {
+                    exceptions.authenticationEntryPoint((request, response, authException) ->
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+                    );
+                    exceptions.accessDeniedHandler((request, response, accessDeniedException) ->
+                            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied")
+                    );
+                });
+
+        return http.build();
+    }
+}
