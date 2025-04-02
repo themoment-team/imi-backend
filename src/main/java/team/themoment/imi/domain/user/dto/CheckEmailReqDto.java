811b44927c9302dd7ee7d860d31005b9df96690a
KIM TAEEUN <snowykte0426@naver.com>
2025-04-02 17:32:13 +0900

    update: add validation annotations to CheckEmailReqDto for email field

--- src/main/java/team/themoment/imi/domain/user/dto/CheckEmailReqDto.java
+++ src/main/java/team/themoment/imi/domain/user/dto/CheckEmailReqDto.java
@@ -1,8 +1,11 @@
 package team.themoment.imi.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class CheckEmailReqDto {
+    @NotBlank(message = "이메일은 필수입니다.")
     private String email;
 }
