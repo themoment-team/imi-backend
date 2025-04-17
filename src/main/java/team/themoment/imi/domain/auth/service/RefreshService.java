678a00a8fd39186216a9e69b45d75339669ab118
KIM TAEEUN <snowykte0426@naver.com>
2025-04-17 18:26:48 +0900

    chore: remove auth service class

--- src/main/java/team/themoment/imi/domain/auth/service/RefreshService.java
+++
@@ -1,7 +0,0 @@
-package team.themoment.imi.domain.auth.service;
-
-import team.themoment.imi.domain.auth.data.response.LoginResDto;
-
-public interface RefreshService {
-    LoginResDto execute(String refreshToken);
-}
