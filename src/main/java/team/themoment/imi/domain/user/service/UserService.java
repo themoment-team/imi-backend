8616f5bf8fcc2a033c5a5cb533fea419f62f56f0
snowykte0426 <snowykte0426@naver.com>
2025-04-28 08:44:44 +0900

    update: fix email format validation logic in UserService

--- src/main/java/team/themoment/imi/domain/user/service/UserService.java
+++ src/main/java/team/themoment/imi/domain/user/service/UserService.java
@@ -49,7 +49,7 @@ public class UserService {
     }
 
     private boolean isEmailFormat(String email) {
-        return !email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+        return email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
     }
 
     public void updateUserInfo(String name, String email, int studentId) {
