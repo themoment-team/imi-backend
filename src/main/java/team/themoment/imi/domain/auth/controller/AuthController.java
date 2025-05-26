5085fd3685ce273c0b9642d03309c56aa038dd6a
snowykte0426 <snowykte0426@naver.com>
2025-05-27 00:19:07 +0900

    feat: add endpoints for sending and verifying emails

--- src/main/java/team/themoment/imi/domain/auth/controller/AuthController.java
+++ src/main/java/team/themoment/imi/domain/auth/controller/AuthController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.themoment.imi.domain.auth.data.request.LoginReqDto;
 import team.themoment.imi.domain.auth.data.request.RefreshJwtReqDto;
+import team.themoment.imi.domain.auth.data.request.SendEmailReqDto;
+import team.themoment.imi.domain.auth.data.request.VerifyEmailReqDto;
 import team.themoment.imi.domain.auth.data.response.LoginResDto;
 import team.themoment.imi.domain.auth.service.AuthService;
 
@@ -28,5 +30,17 @@ public class AuthController {
     @PostMapping("/refresh")
     public ResponseEntity<LoginResDto> refresh(@Valid @RequestBody RefreshJwtReqDto reqDto) {
         return ResponseEntity.status(HttpStatus.OK).body(authService.refresh(reqDto.refreshToken()));
+    }
+
+    @PostMapping("/send-email")
+    public ResponseEntity<Void> sendEmail(@Valid @RequestBody SendEmailReqDto reqDto) {
+        authService.sendEmail(reqDto.email());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping("/verify-email")
+    public ResponseEntity<Void> verifyEmail(@Valid @RequestBody VerifyEmailReqDto reqDto) {
+        authService.verifyAuthCode(reqDto.authCode());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
