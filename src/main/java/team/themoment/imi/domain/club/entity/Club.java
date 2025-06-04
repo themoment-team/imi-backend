974634e60e54ad00f3e7c19f039e05ab2c465196
snowykte0426 <snowykte0426@naver.com>
2025-06-04 10:28:52 +0900

    update: add size constraint to iconUrl field in Club entity

--- src/main/java/team/themoment/imi/domain/club/entity/Club.java
+++ src/main/java/team/themoment/imi/domain/club/entity/Club.java
@@ -1,9 +1,6 @@
 package team.themoment.imi.domain.club.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
@@ -16,5 +13,6 @@ public class Club {
     private String leader;
     private String mainContent;
     private String notionUrl;
+    @Column(columnDefinition = "VARCHAR(512)")
     private String iconUrl;
 }
