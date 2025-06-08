15bcc7ea4b4cd2693e40703c7630416fe452b29b
snowykte0426 <snowykte0426@naver.com>
2025-06-08 14:25:04 +0900

    feat: enhance validation messages in request DTOs for better user feedback

--- src/main/java/team/themoment/imi/domain/auth/data/request/LoginReqDto.java
+++ src/main/java/team/themoment/imi/domain/auth/data/request/LoginReqDto.java
@@ -6,7 +6,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record LoginReqDto(
-        @NotBlank @Email @Size(max = 16, min = 16) String email,
-        @NotBlank @Size(max = 16, min = 8) String password
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "이메일 형식이 아닙니다.")
+        @Size(max = 16, min = 16, message = "이메일은 16자리여야 합니다.")
+        String email,
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Size(max = 16, min = 8, message = "비밀번호는 8자 이상 16자 이하여야 합니다")
+        String password
 ) {
 }
