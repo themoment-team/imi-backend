6e61b948ff107bdf8ff474f9f8a3050ef12b3e45
snowykte0426 <snowykte0426@naver.com>
2025-06-19 23:41:17 +0900

    feat: update deploy.sh to add Docker permissions check and improve container management

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,13 @@ set -e
 
 COMPOSE_FILE=compose.yaml
 
-echo "1) 기존 컨테이너 중지 및 삭제"
-docker compose -f $COMPOSE_FILE down --rmi all --volumes --remove-orphans
+echo "1) Docker 권한 확인"
+sudo chmod 666 /var/run/docker.sock
 
-echo "2) 이미지 빌드 및 컨테이너 실행"
-docker compose -f $COMPOSE_FILE up -d --build
+echo "2) 기존 컨테이너 중지 및 삭제"
+sudo docker compose -f $COMPOSE_FILE down --rmi local --volumes --remove-orphans || true
 
-echo "3) deploy script end"
+echo "3) 이미지 빌드 및 컨테이너 실행"
+sudo docker compose -f $COMPOSE_FILE up -d --build
+
+echo "4) deploy script end"
