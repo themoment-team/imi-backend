48bf5350610d10732cbf6de365c48a9d4204b01e
snowykte0426 <snowykte0426@naver.com>
2025-06-19 23:46:57 +0900

    feat: update deploy.sh to use docker-compose for container management

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,9 @@ echo "1) Docker 권한 확인"
 sudo chmod 666 /var/run/docker.sock
 
 echo "2) 기존 컨테이너 중지 및 삭제"
-sudo docker compose -f $COMPOSE_FILE down --rmi local --volumes --remove-orphans || true
+sudo docker-compose -f $COMPOSE_FILE down --rmi local --volumes --remove-orphans || true
 
 echo "3) 이미지 빌드 및 컨테이너 실행"
-sudo docker compose -f $COMPOSE_FILE up -d --build
+sudo docker-compose -f $COMPOSE_FILE up -d --build
 
 echo "4) deploy script end"
