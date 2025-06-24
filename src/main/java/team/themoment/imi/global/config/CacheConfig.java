fa096a11eac615e2d963d6fa176d7cdc233275f5
snowykte0426 <snowykte0426@naver.com>
2025-06-24 11:43:17 +0900

    feat: enable caching in CacheConfig for improved performance

--- src/main/java/team/themoment/imi/global/config/CacheConfig.java
+++ src/main/java/team/themoment/imi/global/config/CacheConfig.java
@@ -1,6 +1,7 @@
 package team.themoment.imi.global.config;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Collections;
 
 @Configuration
+@EnableCaching
 public class CacheConfig {
 
     @Bean
