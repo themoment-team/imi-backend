104d871996e2a0d7582cca26180d81248c077496
snowykte0426 <snowykte0426@naver.com>
2025-06-24 11:42:48 +0900

    feat: add CacheConfig for managing application cache

---
+++ src/main/java/team/themoment/imi/global/config/CacheConfig.java
@@ -0,0 +1,21 @@
+package team.themoment.imi.global.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+        cacheManager.setAllowNullValues(false);
+        cacheManager.setCacheNames(Collections.singleton("clubs"));
+        
+        return cacheManager;
+    }
+}
