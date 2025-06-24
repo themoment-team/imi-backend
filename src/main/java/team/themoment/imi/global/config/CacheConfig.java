bdcf4b35c18ff6538d632666053ba95a05f67194
snowykte0426 <snowykte0426@naver.com>
2025-06-24 11:49:48 +0900

    feat: implement Caffeine cache manager for improved caching strategy

--- src/main/java/team/themoment/imi/global/config/CacheConfig.java
+++ src/main/java/team/themoment/imi/global/config/CacheConfig.java
@@ -1,11 +1,13 @@
 package team.themoment.imi.global.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
 import java.util.Collections;
 
 @Configuration
@@ -13,11 +15,13 @@ import java.util.Collections;
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager() {
-        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
-        cacheManager.setAllowNullValues(false);
+    public CacheManager caffeineCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofHours(24))
+                .maximumSize(100)
+        );
         cacheManager.setCacheNames(Collections.singleton("clubs"));
-        
         return cacheManager;
     }
 }
