650be40f84c07d710139f103711a71fa6f998e16
snowykte0426 <snowykte0426@naver.com>
2025-07-28 19:45:43 +0900

    feat: add UnauthenticatedException for handling unauthenticated user scenarios

--- src/main/java/team/themoment/imi/global/utils/UserUtil.java
+++ src/main/java/team/themoment/imi/global/utils/UserUtil.java
@@ -1,25 +1,27 @@
 package team.themoment.imi.global.utils;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import team.themoment.imi.domain.user.entity.User;
+import team.themoment.imi.domain.user.exception.MemberNotFoundException;
 import team.themoment.imi.domain.user.repository.UserJpaRepository;
-import team.themoment.imi.global.exception.GlobalException;
+import team.themoment.imi.global.utils.exception.UnauthenticatedException;
 
 @Component
 @RequiredArgsConstructor
 public class UserUtil {
+
     private final UserJpaRepository userJpaRepository;
-    public User getCurrentUser(){
+
+    public User getCurrentUser() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if(principal instanceof UserDetails userDetails){
+        if (principal instanceof UserDetails userDetails) {
             return userJpaRepository.findByEmail(userDetails.getUsername())
-                    .orElseThrow(() -> new GlobalException("Login User not found", HttpStatus.NOT_FOUND));
-        }else {
-            throw new GlobalException("User is not authenticated", HttpStatus.UNAUTHORIZED);
+                    .orElseThrow(MemberNotFoundException::new);
+        } else {
+            throw new UnauthenticatedException();
         }
     }
 }
