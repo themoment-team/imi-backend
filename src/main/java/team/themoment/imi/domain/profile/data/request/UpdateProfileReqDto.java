f98f0b0c7e2a5d406a3341fee4930a171d7af691
snowykte0426 <snowykte0426@naver.com>
2025-09-03 15:25:53 +0900

    feat: update major field validation to limit size to 50 characters

--- src/main/java/team/themoment/imi/domain/profile/data/request/UpdateProfileReqDto.java
+++ src/main/java/team/themoment/imi/domain/profile/data/request/UpdateProfileReqDto.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public record UpdateProfileReqDto(
         @NotNull(message = "희망전공은 필수입니다.")
-        @Size(max = 100)
+        @Size(max = 50, message = "희망전공은 최대 50자까지 입력할 수 있습니다.")
         String major,
         @NotNull(message = "자기소개는 필수입니다.")
         @Size(max = 2400, message = "자기소개는 최대 2400자까지 입력할 수 있습니다.")
